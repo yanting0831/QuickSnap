564373fdd9e88d57c2d412feb6d46d4967c68e54
Yan Ting <101209183@students.swinburne.edu.my>
2019-09-23 12:04:43 +0800

    add sound effects

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -32,15 +32,20 @@ namespace CardGames
 				if ( SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
 				 SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
 				{
-				 //TODO: add sound effects
+					SwinGame.LoadSoundEffectNamed("slap","slap.wav");
+					SwinGame.PlaySoundEffect("slap");
 				}
 				else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT))
 				{
-				myGame.PlayerHit (0);
+					SwinGame.LoadSoundEffectNamed("slap","slap.wav")
+					SwinGame.PlaySoundEffect("slap");
+					myGame.PlayerHit (0);
 				}
 				else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
 				{
-				myGame.PlayerHit (1);
+					SwinGame.LoadSoundEffectNamed("slap","slap.wav")
+					SwinGame.PlaySoundEffect("slap");
+					myGame.PlayerHit (1);
 				}
 			}
 		}
